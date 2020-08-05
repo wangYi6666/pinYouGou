e7635bbcd42f2398b7ced91496617ed688f5f3c8
wangYi6666 <2661561799@qq.com>
2020-08-05 13:33:33 +0800

    添加配置初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <span>欢迎启动</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+  span {
+    font-size: 20px;
+  }
+}
+</style>
